7f5af2fb82ead5379069464eca2f99c243c3c0c3
pcallipari <84109546+pcallipari@users.noreply.github.com>
2022-12-07 20:26:45 +1100

    Completed Day 6 Part 1

---
+++ Day 6/main.py
@@ -0,0 +1,29 @@
+# input_file = r"Day 6\test_input.txt"
+input_file = r"Day 6\my_input.txt"
+
+
+def check_doubles(input_list):
+    for index, value in enumerate(input_list, start=1):
+        if value in input_list[index:]:
+            return False
+    return True
+
+
+def main_part1():
+    with open(input_file, "r") as f:
+
+        temp_array = [*f.read(4)]
+        i = 5
+        while True:
+            next_character = f.read(1)
+            temp_array = temp_array[1:]
+            temp_array.append(next_character)
+            if check_doubles(temp_array):
+                print(f"The first marker point is at i={i}.")
+                break
+
+            i += 1
+
+
+if __name__ == "__main__":
+    main_part1()
