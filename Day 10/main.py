9f54b2a7a28a3b511c05e6026864563a4c2b081c
pcallipari <84109546+pcallipari@users.noreply.github.com>
2022-12-12 15:27:21 +1100

    Completed Day 10 Part 1

---
+++ Day 10/main.py
@@ -0,0 +1,37 @@
+# input_filename = r"Day 10/test_input.txt"
+input_filename = r"Day 10/my_input.txt"
+
+
+def main_part1():
+    with open(input_filename, "r") as f:
+        input_lines = f.read().splitlines()
+
+    current_cycle = 0
+    X_register = 1
+
+    checkpoints = [20, 60, 100, 140, 180, 220]
+    checkpoints.reverse()
+    current_sum = 0
+
+    for command in input_lines:
+        if command.startswith("noop"):
+            current_cycle += 1
+            if current_cycle == checkpoints[-1]:
+                current_sum += checkpoints.pop() * X_register
+
+        elif command.startswith("addx"):
+            if checkpoints[-1] <= current_cycle + 2:
+                current_sum += checkpoints.pop() * X_register
+
+            add_value = int(command.split(" ")[1])
+            X_register += add_value
+            current_cycle += 2
+
+        if len(checkpoints) == 0:
+            break
+
+    print(f"The final sum is: {current_sum}")
+
+
+if __name__ == "__main__":
+    main_part1()
